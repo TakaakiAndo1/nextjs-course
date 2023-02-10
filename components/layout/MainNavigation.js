7404b8d4ca67c93f4419fa2dfa11302ec403c9b1
Takaaki Ando <z.takaaki.ando@gree.net>
2023-02-10 22:07:07 +0900

    ロゴの方にHome画面のリンクをつけるよう変更

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -5,12 +5,11 @@ import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <Link href="/">
+        <div className={classes.logo}>React Meetups</div>
+      </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/">All Meetups</Link>
-          </li>
           <li>
             <Link href="/new-meetup">Add New Meetup</Link>
           </li>
